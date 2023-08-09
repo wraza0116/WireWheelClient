88a38e9a2da95648d9a0029ecbf0a0ded89e804e
Wasim Raza <wasim.786raza@gmail.com>
2023-08-09 18:03:45 +0530

    Added Button to open SDK

--- WireWheelClient/ViewController.swift
+++ WireWheelClient/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func btnOpenSDKPressedAction(_ sender:UIButton) {
+        print("btnOpenSDKPressedAction")
+    }
 
 
 }
