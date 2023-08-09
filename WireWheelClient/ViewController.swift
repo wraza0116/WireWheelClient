9f6927b4a7053c0a7cd0cef3c652b0edeff1576d
Wasim Raza <wasim@simpalm.com>
2023-08-09 16:37:15 +0530

    Initial Commit

---
+++ WireWheelClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WireWheelClient
+//
+//  Created by Simpalm iOS on 09/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WireWheelClientUITests/WireWheelClientUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WireWheelClientUITestsLaunchTests.swift
+//  WireWheelClientUITests
+//
+//  Created by Simpalm iOS on 09/08/23.
+//
+
+import XCTest
+
+final class WireWheelClientUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
